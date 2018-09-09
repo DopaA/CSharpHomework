23cb1e6c6414a13024dfb7b5f497bf0855916f58
DopaA <2534423312@qq.com>
2018-09-09 23:30:42 +0800

    第一次作业提交
    c#的第一次作业提交

---
+++ apdo_homework_1/apdo_homework_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apdo_homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            int divisor_1 = 0, divisor_2 = 0;
+            Console.Write("please enter a divisor:");
+            s = Console.ReadLine();
+            divisor_1 = Int32.Parse(s);
+            Console.Write("please enter another divisor:");
+            s = Console.ReadLine();
+            divisor_2 = Int32.Parse(s);
+            Console.WriteLine("the product of the two numbers is" + divisor_1 * divisor_2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ apdo_homework_1/apdo_homework_1_2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace apdo_homework_1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = " ";
+            s = textBox1.Text;
+            double divisor1 = double.Parse(s);
+            s = textBox2.Text;
+            double divisor2 = double.Parse(s);
+            label1.Text = "divisor1*divisor2 = " + divisor1 * divisor2;
+        }
+    }
+}

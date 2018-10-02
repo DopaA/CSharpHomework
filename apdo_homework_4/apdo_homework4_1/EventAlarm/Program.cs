1a15eff179b18e3daf090e57a293605351f43b40
DopaA <2534423312@qq.com>
2018-10-03 01:03:45 +0800

    第四次作业提交
    第四次c#作业提交

---
+++ apdo_homework_4/apdo_homework4_1/EventAlarm/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventAlarm
+{
+    //声明参数类型
+    public class AlarmArgs : EventArgs
+    {
+        public string Hour;
+        public string Minute;
+    }
+    //声明委托类型
+    public delegate void EventHandler(object sender, AlarmArgs e);
+
+    //声明事件源
+    public class Alarm
+    {
+        //声明事件
+        public event EventHandler Alarming;
+        
+        public void OnRemind(string hour,string minute) 
+        {
+            AlarmArgs alarmArgs = new AlarmArgs
+            {
+                Hour = hour,
+                Minute = minute
+            };
+            Alarming(this, alarmArgs);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var alarm = new Alarm();
+                Console.WriteLine("请输入闹钟时间的小时：");
+                string hour = Console.ReadLine();
+                Console.WriteLine("请输入闹钟时间的分钟：");
+                string minute = Console.ReadLine();
+                //注册事件
+                alarm.Alarming += Remind;
+                alarm.OnRemind(hour, minute);
+            }
+            catch
+            {
+                throw new Exception("Unknown error!");
+            }
+            Console.ReadLine();
+        }
+        //事件处理
+        static void Remind(object sender,AlarmArgs e)
+        {
+            bool bo = true;
+            while (bo)
+            {
+                if (e.Hour == DateTime.Now.Hour.ToString() && e.Minute == DateTime.Now.Minute.ToString())
+                {
+                    Console.WriteLine("The time has come! Now is" + DateTime.Now.ToShortTimeString() + "!");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ apdo_homework_4/apdo_homework4_2/Order/OrderService.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//订单的添加，删除，查找，修改操作
+namespace Order
+{
+    class OrderService
+    {
+        List<Order> orders = new List<Order>();
+        //添加订单
+        public void addOrder(Order order)
+        {
+            orders.Add(order);
+        }
+        //删除订单
+        public void deleteOrder(string sName)
+        {
+            foreach (Order o in orders)
+            {
+                if(o.Name==sName)
+                orders.Remove(o);
+            }
+        }
+        //改变订单内容
+        public void modifyOrder(int num, Order order)
+        {
+            orders.RemoveAt(num);
+            orders.Insert(num, order);
+        }
+        //查询订单
+        //按订单号
+        public void findByOrder(int num)
+        {
+            bool bo = true;
+            foreach (Order o in orders)
+            {
+                if (o.Num == num)
+                {
+                    Console.Write(o.Num + "   " + o.Name + "  " + o.Person + "   " + o.Count + "    " + o.Price + "\n");
+                    bo = false;
+                }
+            }
+            if (bo)
+            {
+                Console.WriteLine("没有所要查找的订单号！");
+            }
+        }
+        //按商品名
+        public void findByName(string name)
+        {
+            bool bo = true;
+            foreach(Order o in orders)
+            {
+                if (o.Name == name)
+                {
+                    Console.Write(o.Num + "   " + o.Name + "  " + o.Person + "   " + o.Count + "    " + o.Price + "\n");
+                    bo = false;
+                }
+            }
+            if (bo)
+            {
+                Console.WriteLine("没有所要查找的商品名！");
+            }
+        }
+        //按客户
+        public void findByPerson(string person)
+        {
+            bool bo = true;
+            foreach (Order o in orders)
+            {
+                if (o.Person == person)
+                {
+                    Console.Write(o.Num + "   " + o.Name + "  " + o.Person +"   "+ o.Count + "    " + o.Price + "\n");
+                    bo = false;
+                }
+            }
+            if (bo)
+            {
+                Console.WriteLine("没有所要查找的客户！");
+            }
+        }
+        //返回索引
+        public int GetNumByName(string sName)
+        {
+            int i = 0;
+            foreach (Order o in orders)
+            {
+                if(o.Name==sName)
+                return i;
+                i++;
+            }
+            return -1;
+        }
+    }
+}

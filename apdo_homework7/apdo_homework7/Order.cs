4a672d361272f5521ce9649dd12dc3c39434d377
DopaA <2534423312@qq.com>
2018-10-27 16:14:14 +0800

    c#第七次作业
    c#第七次作业提交

---
+++ apdo_homework7/apdo_homework7/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//订单的组成部分,以及订单的生成
+namespace Orders
+{
+    [Serializable]
+    public class Order
+    {
+        public int OrderNum { set; get; }//订单编号
+        public string OrderName { set; get; }//订单商品名
+        public string OrderPerson { set; get; }//订单的客户名
+        public int OrderCount { set; get; }//订单数量
+        public int OrderPrice { set; get; }//订单单个价格
+        public Order() { }
+        public Order(int OrderNum, string OrderName, string OrderPerson, int OrderCount, int OrderPrice)
+        {
+            this.OrderNum = OrderNum;
+            this.OrderName = OrderName;
+            this.OrderPerson = OrderPerson;
+            this.OrderCount = OrderCount;
+            this.OrderPrice = OrderPrice;
+        }
+        public override string ToString()
+        {
+            return OrderName + OrderNum + OrderPerson + OrderCount + OrderPrice;
+        }
+    }
+}
---
+++ apdo_homework7/apdo_homework_form7/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Orders;
+namespace apdo_homework_form7
+{
+    public partial class Form1 : Form
+    {
+        public int numOfOrder = 0;
+        public string sName;
+        public string sPerson;
+        public int count;
+
+        string OrderName;
+        List<Order> orders = new List<Order>();
+        public string KeyWord { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            bindingSource1.DataSource = orders;
+            textBox1.DataBindings.Add("Text", this, "KeyWord");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+           bindingSource1.Add(new Order(numOfOrder, sName, sPerson, count, 1001));
+            DialogResult result = MessageBox.Show("创建成功", "提示框", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            sName = this.textBox3.Text;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            sPerson = this.textBox4.Text;
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            string s = this.textBox5.Text;
+            count = Int32.Parse(s);
+        }
+        //删除按钮
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < orders.Count; i++)
+            {
+                if (orders[i].OrderName == OrderName)
+                    bindingSource1.Remove(orders[i]);
+            }
+        }
+        //查询按钮
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text != "")
+            {
+                bindingSource1.DataSource =
+                    orders.Where(o => o.OrderName == KeyWord);
+            }
+            else
+            {
+                bindingSource1.DataSource = orders;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            OrderName = this.textBox2.Text;        
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {        
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+    }
+}
